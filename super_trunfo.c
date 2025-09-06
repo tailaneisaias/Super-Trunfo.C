40f831b78c7acd2c56000d2839180fa8e51f490c
Tailane Isaias <tailaneisaias08@gmail.com>
2025-09-06 12:09:39 -0300

    adicionando as ultimas impressões

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-  char Nome, Estado[50], CodigoDaCarta[50], NomeDaCidade[50];
+  char Nome, Estado, CodigoDaCarta[20], NomeDaCidade[50];
   int Populacao, NumeroDePontosTuristicos;
   float Area, PIB;
 
-  printf("Carta1 \n", Nome);
+  printf("Carta1 \n");
 
   printf("Uma letra que represente seu estado: \n");
-  scanf("%s", &Estado);
+  scanf("%c", &Estado);
 
   printf("Digite a letra do seu estado, junto com um número de 01 a 04 (ex: C01, B02) \n");
   scanf("%s", &CodigoDaCarta);
@@ -17,6 +17,19 @@ int main()
   printf("Nome da sua cidade: \n");
   scanf("%s", &NomeDaCidade);
 
-  printf("-Estado: %s \n-CodigoDaCarta: %s \n-NomeDaCidade: %s \n", Estado, CodigoDaCarta, NomeDaCidade);
+  printf("O número de habitantes da sua cidade: \n");
+  scanf("%d", &Populacao);
+
+  printf("A aréa da cidade em quilometros quadrados \n");
+  scanf("%f", &Area);
+
+  printf("O produto interno Bruto da cidade \n");
+  scanf("%f", &PIB);
+
+  printf("A quantidade de pontos turisticos da sua cidade: \n");
+  scanf("%i", &NumeroDePontosTuristicos);
+
+  printf("-Estado: %c \n-CodigoDaCarta: %s \n-NomeDaCidade: %s \n-População: %i \n", Estado, CodigoDaCarta, NomeDaCidade, Populacao);
+  printf("-Area: %f \n-PIB: %f \n-NúmeroDePontosTurísticos: %i \n", Area, PIB, NumeroDePontosTuristicos);
   return 0;
 }
